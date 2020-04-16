54645023250260f2d57fdd3be2ad0310c70424f7
ravinWijekoon <ravinmalingawijekoon@gmail.com>
2020-04-16 22:17:09 +0530

    readAppointmentsForSpecifiedDoctors function added

--- src/com/HealthcareSystem/service/doctor/doctorService.java
+++ src/com/HealthcareSystem/service/doctor/doctorService.java
@@ -92,6 +92,7 @@ public class doctorService {
 	@Produces(MediaType.TEXT_HTML)
 	public String readAppointmentsForSpecifiedDoctors(@PathParam("id") String d_id) {
 		return Obj.readAppointmentsForSpecifiedDoctors(d_id);
+		//gets appointment details
 	}
 
 }
